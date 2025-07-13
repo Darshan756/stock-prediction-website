2d10279624d6d3e51c65a2c14c069e2ee9367dba
Darshan MT <darshanmalli93@gmail.com>
2025-07-13 11:24:40 +0530

    created registration api in drf

---
+++ backend/account/views.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response 
+from rest_framework import generics 
+from django.contrib.auth.models import User
+from .serializers import UserAccountSerializer
+from rest_framework.permissions import AllowAny
+# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserAccountSerializer
+    permission_classes = [AllowAny]
+
+  
+        
+        
+      
+
