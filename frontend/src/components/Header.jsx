23dad5c57ff423bef40af25c63becea98bfdd3b1
Darshan MT <darshanmalli93@gmail.com>
2025-07-13 22:28:00 +0530

    login ,logout form is created and localstorage used to store jwt tokens

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import {useContext} from 'react'
 //components
 import Button from './Button'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
+import { AuthContext } from './AuthProvider'
 
 const Header = () => {
+  const {isLoggedIn,setIsLoggedIn} = useContext(AuthContext)
+  const navigate = useNavigate();
+ 
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('refreshToken')
+    setIsLoggedIn(false)
+    navigate('/login')
+   
+  }
   return (
     <>
     <nav className='navbar container pt-3 pb-3 align-items-start'>
         <Link className='navbar-brand text-light' to="/">Stock Prediction Portal</Link>
-        <div>
+        <div>{isLoggedIn  ? <button className='btn btn-outline-danger ' onClick={handleLogout}>Logout</button> : (
+          <>
             <Button className='btn btn-outline-info' url="/login" >Login</Button>
             &nbsp;
             <Button className='btn btn-info' url='/register'>Register</Button>
+            </>
+        )}
         </div>
       </nav>
     </>
